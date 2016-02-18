e9f4c744d1e70904e0da7bb93062a2ab00844ab4
Wetteren Rémi <r.wetteren@gmail.com>
2016-02-18 12:19:32 +0100

    Mise en place des tests unitaires

---
+++ src/ChatBundle/UnitTests/Controller/UsersControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Calderis
+ * Date: 18/02/2016
+ * Time: 12:11
+ */
+namespace ChatBundle\UnitTests\Controller;
+use ChatBundle\Controller\UsersController;
+
+class UsersControllerTest extends \PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function first()
+    {
+        // Arrange
+        $a = new UsersController();
+    }
+
+    // ...
+}
---
+++ src/ChatBundle/UnitTests/Form/UsersTypeTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Calderis
+ * Date: 18/02/2016
+ * Time: 12:12
+ */
+namespace ChatBundle\UnitTests\Form;
+use ChatBundle\Form\UsersType;
+
+class UsersTypeTest extends \PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function first()
+    {
+        // Arrange
+        $a = new UsersType();
+    }
+
+    // ...
+}
